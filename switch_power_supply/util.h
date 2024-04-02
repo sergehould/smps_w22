e8f844891954700da79bf6636d7d3d80094ca10e
sergehould <houlds@vaniercollege.qc.ca>
2024-04-02 11:38:00 -0400

    Update util.h

--- switch_power_supply/util.h
+++ switch_power_supply/util.h
@@ -12,7 +12,8 @@
 #ifdef	__cplusplus
 extern "C" {
 #endif
-#define heartbeat_init  init_heartbeat
+#define 	heartbeat_init  init_heartbeat
+#define		tone_init			init_tone
 
 void heartbeat(void);
 void init_heartbeat(void);
@@ -21,6 +22,9 @@ void tone_low(int cnt);
 void init_tone(void);
 void delay_ms( unsigned int);
 void delay_10us( unsigned int);
+long map(long x, long in_min, long in_max, long out_min, long out_max);
+float mapf(float x, float in_min, float in_max, float out_min, float out_max);
+
 
 #ifdef	__cplusplus
 }
